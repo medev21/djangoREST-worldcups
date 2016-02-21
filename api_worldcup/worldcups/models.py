a3b48cee34bb804ce79b099e57e85b4762e3f7cc
Martin Ben <mebenav88@gmail.com>
2016-02-21 15:44:10 -0500

    add params to Worldcup model

--- api_worldcup/worldcups/models.py
+++ api_worldcup/worldcups/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Worldcup(models.Model):
+    created = models.DateTimeField(auto_now_add = True) #created date time
+    title = models.CharField(max_length = 100, blank = True, default = '')
+    description = models.TextField()
+    year = models.IntegerField(default = 2016)
+    winner = models.CharField(max_length =100, blank = True, default = '')
+    runner_up = models.CharField(max_length = 100, blank = True, default = '')
+    owner = models.ForeignKey('auth.User', related_name = 'worldcups')
+
+    class Meta:
+        ordering = ('created',)
