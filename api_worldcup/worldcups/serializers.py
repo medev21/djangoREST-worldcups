1ea3ec97d5623a712c8949674a9e9fd59fd7504d
Martin Ben <mebenav88@gmail.com>
2016-02-21 16:07:51 -0500

    add serializer

---
+++ api_worldcup/worldcups/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from worlcups.models import Worldcup
+from django.contrib.auth.models import User
+
+class WorlcupSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source = 'owner.username')
+
+    class Meta:
+        model = Worldcup
+        fields = ('url', 'owner', 'title', 'description', 'year', 'winner', 'runner_up')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    worldcups = serializers.HyperlinkedRelatedField(many = True, view_name = 'worlcup-detail', read_only = True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'worldcups')
