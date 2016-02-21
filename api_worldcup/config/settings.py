1529a3323bf2686b3cee4966ab2f69c4834b8f5d
Martin Ben <mebenav88@gmail.com>
2016-02-21 15:26:36 -0500

    start app worldcups and add to installed app in settings, also rest framework

--- api_worldcup/config/settings.py
+++ api_worldcup/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'worldcups',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ api_worldcup/worldcups/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WorldcupsConfig(AppConfig):
+    name = 'worldcups'
